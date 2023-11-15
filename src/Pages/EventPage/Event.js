4926e92acea10610e43e3a9f7623839fdbd67d42
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-11-15 22:58:42 +0900

    Change

--- src/Pages/EventPage/Event.js
+++ src/Pages/EventPage/Event.js
@@ -1,58 +1,93 @@
 import React from "react";
 import LayoutWithSidebar from "../../components/LayoutWithSidebar";
 import { Link } from "react-router-dom";
-import { Button, Typography ,Box,Grid} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import { Button, Typography, Box, Grid } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase/firebaseConfig";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebaseConfig";
 
-
-const Event =({EventData, EventTitle})=>{
+const Event = ({ EventData, EventTitle }) => {
   const EventImage = EventData.image;
   const EventTime = EventData.time;
   const EventDesciption = EventData.description;
   const EventHost = EventData.host;
   const EventOccupancy = EventData.occupancy;
-  return(
+  const user = auth.currentUser;
+
+  const navigate = useNavigate();
+
+  const handleJoinEvent = async (event, title) => {
+    const dataToStore = {
+      title: title,
+      eventData: event,
+    };
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const existingEvents = docSnap.data().events;
+      const isEventJoined = existingEvents.some((e) => e.title === title);
+      if (isEventJoined) {
+        alert("You have already joined this event!");
+      } else {
+        const updatedEvents = [...existingEvents, dataToStore];
+        await updateDoc(docRef, { events: updatedEvents });
+        alert("Event Joined!");
+      }
+    } else {
+      console.log("No such document!");
+    }
+  };
+
+  return (
     <LayoutWithSidebar>
       <div>
-      <div style={{ fontSize: '48px', fontWeight: 'bolder' }}>
-  <span>{EventTitle}</span>
-</div>
+        <div style={{ fontSize: "48px", fontWeight: "bolder" }}>
+          <span>{EventTitle}</span>
+        </div>
         <Box
-        sx={{
-          backgroundColor: "skyblue",
-          height: '20vh', // 높이를 화면의 50%로 설정 혹은 다른 값으로 조절
-          width: '30%', // 가로 크기를 100%로 설정
-          backgroundImage: `url(${`/images/${EventImage}`})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}/>
-        <div className="EventText" style={{fontSize:'8px',whiteSpace:"pre-line"}}>
-        <h1 style={{fontWeight:'bold'}}>Date & Time:{EventTime}</h1>
-        <h1>{EventDesciption}</h1>
-        <h1>{EventHost}</h1>
-        <h1>{EventOccupancy}</h1>
+          sx={{
+            backgroundColor: "skyblue",
+            height: "20vh", // 높이를 화면의 50%로 설정 혹은 다른 값으로 조절
+            width: "30%", // 가로 크기를 100%로 설정
+            backgroundImage: `url(${`/images/${EventImage}`})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <div
+          className="EventText"
+          style={{ fontSize: "8px", whiteSpace: "pre-line" }}
+        >
+          <h1 style={{ fontWeight: "bold" }}>Date & Time:{EventTime}</h1>
+          <h1>{EventDesciption}</h1>
+          <h1>{EventHost}</h1>
+          <h1>{EventOccupancy}</h1>
         </div>
-        </div>
-        <div>
+      </div>
+      <div>
         <Box maxWidth="30%">
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs={6}>
-          <Link to="/chat">
+          <Grid container spacing={2} alignItems="center">
+            {/* <Grid item xs={6}> */}
+            {/* <Link to="/chat">
             <Button variant="contained" color="primary" fullWidth>
               Message Host
             </Button>
-          </Link>
-        </Grid>
-        <Grid item xs={6}>
-          <Link to="/yourevents">
-            <Button variant="contained" color="primary" fullWidth>
-              Join Event
-            </Button>
-          </Link>
-        </Grid>
-      </Grid>
-    </Box>
-        </div>
+          </Link> */}
+            {/* </Grid> */}
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="primary"
+                fullWidth
+                onClick={() => handleJoinEvent(EventData, EventTitle)}
+              >
+                Join Event
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
     </LayoutWithSidebar>
   );
 };
--- src/Pages/EventPage/KoreanBath.js
+++ src/Pages/EventPage/KoreanBath.js
@@ -1,15 +1,21 @@
 import React from "react";
 import Event from "./Event";
+import { useLocation } from "react-router-dom";
 
-const KoreanBath =()=>{
-  const KoreanBathEventData ={
-    image:"KoreanBath.jpg",
+const KoreanBath = () => {
+  const KoreanBathEventData = {
+    image: "KoreanBath.jpg",
     time: "26 Oct 12:00 - 26 Oct 24:00",
-    description:"Take a bath and drinking banana milk is somewhat fantastic!!\n Let's go to korean bathhouse and clean our tired body and mental\n Come on Guys!",
-    host:"Hosted Kim Min Ju 4.82",
-    occupancy: "Current Occupancy 3/5"
-
+    description:
+      "Take a bath and drinking banana milk is somewhat fantastic!!\n Let's go to korean bathhouse and clean our tired body and mental\n Come on Guys!",
+    host: "Hosted Kim Min Ju 4.82",
+    occupancy: "Current Occupancy 3/5",
   };
-  return <Event EventData={KoreanBathEventData} EventTitle="Visit a Korean BathHouse"/>
+  return (
+    <Event
+      EventData={KoreanBathEventData}
+      EventTitle="Visit a Korean BathHouse"
+    />
+  );
 };
 export default KoreanBath;
